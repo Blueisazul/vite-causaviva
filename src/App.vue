08670d383149f7672dcb1c5300f709dab37de104
Blueisazul <sant.14.go@outlook.com>
2025-04-28 22:52:24 -0500

    new

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import HelloTailwind from './components/HelloTailwind.vue';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
@@ -12,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <HelloTailwind />
 </template>
 
 <style scoped>
