60c9227d35620db1e1e4442a83c268abbfa77c67
Blueisazul <sant.14.go@outlook.com>
2025-04-28 19:14:21 -0500

    gh-pagess

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import ghPages from 'vite-plugin-gh-pages' // <<---- Importa bien el plugin
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), ghPages()],
-  base: "vite-causaviva"
+  plugins: [vue(), ghPages()], // <<---- Ahora sí puedes usar ghPages()
+  base: '/vite-causaviva/',     // <<---- Corrige también base
 })
